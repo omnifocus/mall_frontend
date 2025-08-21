73eb4f3cbaa7287521f05bf82ad8d082809b6f24
panda <panda@pandas-MacBook-Pro.local>
2025-08-21 13:35:08 -0700

    menu retrieve done

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'subList',
+        label: 'name',
+      },
+    }
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data)
+    },
+    loadCategory() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/treelist'),
+        method: 'get',
+      }).then(({ data }) => {
+        console.log('data', data)
+        this.data = data.list
+      })
+    },
+  },
+  mounted() {
+    console.log('loading....')
+    this.loadCategory()
+  },
+}
+</script>
+
+<style scoped></style>
