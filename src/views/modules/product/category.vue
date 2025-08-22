5893dc73d3f211e5ed3743b90baa3e364c98ce5c
lei <thenextlei@gmail.com>
2025-08-22 16:44:42 -0700

    product category add done

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,33 +1,71 @@
 <template>
-  <el-tree
-    :data="data"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-    :expand-on-click-node="false"
-    :default-expanded-keys="expandedKeys"
-    node-key="catId"
-    show-checkbox
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="data.subList.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+      :expand-on-click-node="false"
+      :default-expanded-keys="expandedKeys"
+      node-key="catId"
+      show-checkbox
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="data.subList.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            删除
+          </el-button>
+          <el-button
+            v-if="data.catLevel <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(node, data)"
+          >
+            添加
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+    <el-dialog
+      :close-on-click-modal="false"
+      title="添加商品"
+      :visible.sync="dialogVisible"
+      width="30%"
+    >
+      <el-form ref="categoryForm" :model="categoryForm" label-width="120px">
+        <el-form-item label="商品名称">
+          <el-input v-model="categoryForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="商品图标">
+          <el-input v-model="categoryForm.icon"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="categoryForm.productUnit"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="saveCategory">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      categoryForm: {
+        name: '',
+        icon: '',
+        productUnit: '',
+      },
+      dialogVisible: false,
       expandedKeys: [],
       data: [],
       defaultProps: {
@@ -49,13 +87,13 @@ export default {
         this.data = data.list
       })
     },
-    append(data) {
-      console.log('data', data)
-      const newChild = { id: id++, label: 'testtest', children: [] }
-      if (!data.children) {
-        this.$set(data, 'children', [])
-      }
-      data.children.push(newChild)
+    append(node, data) {
+      this.dialogVisible = true
+      // 补全数据
+      this.categoryForm.parentCid = data.catId
+      this.categoryForm.catLevel = data.catLevel + 1
+      this.categoryForm.showStatus = 1
+      this.categoryForm.sort = 0
     },
 
     remove(node, data) {
@@ -83,6 +121,25 @@ export default {
             this.$message.error(data.msg)
           }
         })
+      })
+    },
+    saveCategory() {
+      this.dialogVisible = false
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.categoryForm,
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+          })
+          this.expandedKeys = [this.categoryForm.parentCid]
+          this.loadCategory()
+        } else {
+          this.$message.error(data.msg)
+        }
       })
     },
   },
