b5d0f4798fd926033ae9b851d825e029c7436462
panda <panda@pandas-MacBook-Pro.local>
2025-08-22 10:24:47 -0700

    before adding product category delete function

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,26 @@
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="data"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+    :expand-on-click-node="false"
+    node-key="catId"
+    show-checkbox
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="data.subList.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
+          Delete
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -25,6 +46,24 @@ export default {
         console.log('data', data)
         this.data = data.list
       })
+    },
+    append(data) {
+      console.log('data', data)
+      const newChild = { id: id++, label: 'testtest', children: [] }
+      if (!data.children) {
+        this.$set(data, 'children', [])
+      }
+      data.children.push(newChild)
+    },
+
+    remove(node, data) {
+      console.log('node', node)
+      console.log('data', data)
+
+      const parent = node.parent
+      const children = parent.data.children || parent.data
+      const index = children.findIndex(d => d.id === data.id)
+      children.splice(index, 1)
     },
   },
   mounted() {
