44330ffbc208c6f39813381609ce28e3ad584dc0
lei <thenextlei@gmail.com>
2025-08-22 22:24:21 -0700

    done

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,27 +1,61 @@
 <template>
-  <el-tree
-    :data="data"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-    :expand-on-click-node="false"
-    :default-expanded-keys="expandedKeys"
-    node-key="catId"
-    show-checkbox
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="data.subList.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+      :expand-on-click-node="false"
+      :default-expanded-keys="expandedKeys"
+      node-key="catId"
+      show-checkbox
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="data.subList.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+          <el-button
+            v-if="data.catLevel <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(node, data)"
+          >
+            Add
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+
+    <el-dialog
+      title="添加"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+      :close-on-click-modal="false"
+    >
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="icon">
+          <el-input v-model="form.icon"></el-input>
+        </el-form-item>
+        <el-form-item label="productUnit">
+          <el-input v-model="form.productUnit"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="submitForm">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -34,6 +68,8 @@ export default {
         children: 'subList',
         label: 'name',
       },
+      dialogVisible: false,
+      form: {},
     }
   },
   methods: {
@@ -49,13 +85,33 @@ export default {
         this.data = data.list
       })
     },
-    append(data) {
-      console.log('data', data)
-      const newChild = { id: id++, label: 'testtest', children: [] }
-      if (!data.children) {
-        this.$set(data, 'children', [])
-      }
-      data.children.push(newChild)
+    append(node, data) {
+      console.log('append:data', data)
+      this.dialogVisible = true
+      this.form.parentCid = data.catId
+      this.form.catLevel = data.catLevel + 1
+      this.form.showStatus = 1
+      this.form.sort = 0
+    },
+
+    submitForm() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.form,
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: '操作成功',
+            type: 'success',
+          })
+          this.expandedKeys = [this.form.parentCid]
+          this.dialogVisible = false
+          this.loadCategory()
+        } else {
+          this.$message.error(data.msg)
+        }
+      })
     },
 
     remove(node, data) {
